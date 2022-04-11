87ab478d63e9f7bf2474457583e5841aff9dec97
xjbeta <xjbeta.tiny@gmail.com>
2022-04-11 14:35:31 +0800

    Initial Commit

---
+++ dylib checker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  dylib checker
+//
+//  Created by xjbeta on 4/11/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
